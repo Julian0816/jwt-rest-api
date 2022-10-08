24178999385681035c6f27f0401932455d87af0b
Julian Martinez <julian.martinez.cs@gmail.com>
2022-10-08 14:09:23 +0100

    set up and server completed

---
+++ sever.js
@@ -0,0 +1,16 @@
+const Express = require('express');
+const bodyParser = require('body-parser');
+const authRouter = require('./src/user-router');
+
+let server =  new Express();
+server.use(bodyParser.json());
+server.use((req, res, next) => {
+    console.log('----- reqqq -----', req.url, req.body);
+    next();
+});
+
+server.use('/user', authRouter);
+
+server.listen(3000, () => {
+    console.log('Express server listening at port : ', 3000)
+})
